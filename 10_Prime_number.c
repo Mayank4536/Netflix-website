6e5298829277fc0c17781879ff8dff76e4f2f024
mayank-demo <157868177+Mayank4536@users.noreply.github.com>
2024-02-28 10:55:51 +0530

    first commit

---
+++ 10_Prime_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,prime=0;
+    printf("Enter a number \n");
+    scanf("%d",&n);
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0){
+          prime=1;
+          break;
+        }
+    }
+    if(prime==1){
+        printf("This is not a prime number");
+    }
+    else{
+        printf("This is a prime number");
+    }
+    return 0;
+}
